f0a8dd93bda2f482ca009290464620d8edcc2274
Brokkoli11 <varszegi.mark111@gmail.com>
2021-01-21 18:17:46 +0100

    first commit

---
+++ NeptunClicker.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Neptun Clicker
+// @namespace    https://github.com/imdonix/canvas-antidetect
+// @version      1.0
+// @downloadURL  https://github.com/imdonix/canvas-antidetect/raw/master/ad.user.js
+// @description  Click "Hallgatói web" on elte neptun site.
+// @author       Brokkoli11
+// @include      https://neptun.elte.hu/*
+// @exclude      https://neptun.elte.hu/ToNeptunWeb/ToNeptunHWeb
+// @grant        none
+// ==/UserScript==
+
+
+
+(function() {
+
+        var button = document.querySelector('body > header > nav > div > a.nav-link.pl-3.pr-0');
+        button.click();
+}
+
+)();
